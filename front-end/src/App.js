517beac16088869735c8f6a5e015bfd8e43ee958
Denislav Velichkov <denislav.velichkov@ffwagency.com>
2020-12-24 12:08:20 +0200

    About page and Main page added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import About from "./components/About/About";
 
 const App = () => {
 
@@ -17,8 +18,33 @@ const App = () => {
                 <header><Nav/></header>
                 <main>
                     <Switch>
+                        <Route path="/">
+                            <h1>Browser</h1>
+                            <form action="" method="">
+                                <input type="text"
+                                       className="search"
+                                       name="search"
+                                       value="{{search}}"
+                                       placeholder="Search..."/>
+                                <input type="number"
+                                       name="from"
+                                       className="difficulty"
+                                       value=""
+                                       placeholder="Difficulty from..."/>
+                                <span>-</span>
+                                <input type="number"
+                                       name="to"
+                                       className="difficulty"
+                                       value=""
+                                       placeholder="Difficulty to..."/>
+                                <input type="submit" value="search"/>
+                            </form>
+                        </Route>
                         <Route path="/cube">
                             <Cube/>
+                        </Route>
+                        <Route path="/about">
+                            <About/>
                         </Route>
                     </Switch>
                 </main>
